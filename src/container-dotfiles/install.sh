80cb0ed9f00261dd809c84eaf0eeedd02c88b2e8
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-29 16:18:57 -0400

    # Remove Azure CLI Persistence NOTES.md and update devcontainer-feature.json, install.sh
    # What:
    # Removed the NOTES.md file from src/azure-cli-persistence/ and updated the devcontainer-feature.json and install.sh files in src/container-dotfiles/.
    # Why:
    # The NOTES.md file was no longer needed as its content was already integrated into the devcontainer-feature.json and install.sh files. This simplifies the project structure and reduces redundancy.

--- src/container-dotfiles/install.sh
+++ src/container-dotfiles/install.sh
@@ -6,15 +6,20 @@ su -l $_REMOTE_USER -c "echo 'eval \"\$(oh-my-posh init zsh --config /usr/local/
 # shellcheck disable=SC2016
 su -l $_REMOTE_USER -c "echo 'eval \"\$(oh-my-posh init bash --config /usr/local/share/oh-my-posh/powerlevel10k.omp.json)\"' >> $_REMOTE_USER_HOME/.bashrc"
 
-if [ ! -d $_REMOTE_USER_HOME/.oh-my-zsh/custom ];then
-  mkdir -p $_REMOTE_USER_HOME/.oh-my-zsh/custom && chown $_REMOTE_USER:$_REMOTE_USER $_REMOTE_USER_HOME/.oh-my-zsh/custom
+if command -v az &> /dev/null; then
+  su -l $_REMOTE_USER -c "yes y | az config set auto-upgrade.enable=yes"
+  su -l $_REMOTE_USER -c "yes y | az config set auto-upgrade.prompt=no"
+  if [ ! -d $_REMOTE_USER_HOME/.oh-my-zsh/custom ];then
+    su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.oh-my-zsh/custom && chown $_REMOTE_USER:$_REMOTE_USER $_REMOTE_USER_HOME/.oh-my-zsh/custom"
+  fi
+  su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
 fi
 
-su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
-
-su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.tmux.conf https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.tmux.conf"
-su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.tmux/plugins"
-su -l $_REMOTE_USER -c "git clone https://github.com/tmux-plugins/tpm $_REMOTE_USER_HOME/.tmux/plugins/tpm"
+if command -v tmux &> /dev/null; then
+  su -l $_REMOTE_USER -c "curl -L -o $_REMOTE_USER_HOME/.tmux.conf https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.tmux.conf"
+  su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.tmux/plugins"
+  su -l $_REMOTE_USER -c "git clone https://github.com/tmux-plugins/tpm $_REMOTE_USER_HOME/.tmux/plugins/tpm"
+fi
 
 if command -v /opt/conda/bin/conda &> /dev/null; then
   su -l $_REMOTE_USER -c "/opt/conda/bin/conda init --all"
