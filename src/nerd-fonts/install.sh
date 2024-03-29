b82df5e68040f2149b7a7a42f7ec82f4d2fc8238
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-29 12:17:14 -0400

    perf(nerd-fonts): improve installation script by adding package checks and updating apt packages before installing fonts to ensure a clean environment docs(nerd-fonts): update installation script to run as root and use apt-get instead of curl for package installation build(nerd-fonts): modify devcontainer-feature.json file to include version number, documentation URL, and dependencies information chore(nerd-fonts): refactor installation script by adding functions for apt-get update and package check to improve readability and maintainability

--- src/nerd-fonts/install.sh
+++ src/nerd-fonts/install.sh
@@ -1,9 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-WORKINGDIR=$(pwd)
-DOWNLOADLOCATION=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets[] | select(.name=="Meslo.zip").browser_download_url')
-curl -L -o fonts.zip ${DOWNLOADLOCATION}
-su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.local/share/fonts"
-su -l $_REMOTE_USER -c "unzip ${WORKINGDIR}/fonts.zip -d $_REMOTE_USER_HOME/.local/share/fonts/"
-su -l $_REMOTE_USER -c "fc-cache $_REMOTE_USER_HOME/.local/share/fonts"
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages fontconfig curl
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+#WORKINGDIR=$(pwd)
+#DOWNLOADLOCATION=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets[] | select(.name=="Meslo.zip").browser_download_url')
+#curl -L -o fonts.zip ${DOWNLOADLOCATION}
+#if [ ! -d /usr/share/fonts/NerdFonts ];then
+#    mkdir -p /usr/share/fonts/NerdFonts
+#fi
+
+curl -s https://github.com/ryanoasis/nerd-fonts/blob/master/install.sh | bash -s -- --clean --install-to-system-path Meslo
+
+#unzip ${WORKINGDIR}/fonts.zip -d /usr/share/fonts/NerdFonts"
+#fc-cache /usr/share/fonts
