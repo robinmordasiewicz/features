1a9b1fb7c73528fd085c39a82ab6850cf3d39fc8
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-30 21:43:14 -0400

    chore(container-dotfiles): increment version number in devcontainer-feature.json from 0.0.36 to 0.0.37 docs(container-dotfiles): update install.sh script by removing unnecessary comment and command "locales-all"

--- src/container-dotfiles/install.sh
+++ src/container-dotfiles/install.sh
@@ -36,5 +36,3 @@ if command -v /opt/conda/bin/conda &> /dev/null; then
   su -l $_REMOTE_USER -c "/opt/conda/bin/conda init --all"
   su -l $_REMOTE_USER -c "/opt/conda/bin/conda config --set changeps1 False"
 fi
-
-locales-all
