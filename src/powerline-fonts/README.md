eb8bd11fa9a6f77fc9029f3c82d8ef4892879490
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-30 20:27:03 -0400

    These changes look like they are related to the installation and configuration of various fonts and text editors (Powerline, Vim) for different environments (Docker containers, Ubuntu). The `devcontainer-feature.json` files seem to be defining features for these installations in Dev Containers.
    The changes include:
    
    * Updating the version numbers of some features and scripts.
    * Adding a new script `check_packages` that checks if certain packages are installed and installs them if not, using `apt-get`.
    * Installing fonts (Powerline) for Powerline symbols in Ubuntu.
    * Updating the version number of the Vim feature to 0.0.7.
    
    It's important to note that these changes should be applied carefully and tested thoroughly before deploying them to production environments, as they may introduce new dependencies or configuration options that could impact system stability or security.

---
+++ src/powerline-fonts/README.md
@@ -0,0 +1,24 @@
+
+# Nerd Fonts (nerd-fonts)
+
+Install nerd fonts.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/robinmordasiewicz/features/nerd-fonts:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| fonts | Supply a list of nerd-fonts to install | string | Meslo |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/robinmordasiewicz/features/blob/main/src/nerd-fonts/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
--- src/vimrc/install.sh
+++ src/vimrc/install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
 set -e
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt-get update -y
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages fontconfig fonts-powerline
+
+fc-cache -f /usr/share/fonts
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
 su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.vim/pack/plugin/start"
 su -l $_REMOTE_USER -c "git clone https://github.com/vim-airline/vim-airline $_REMOTE_USER_HOME/.vim/pack/plugin/start/vim-airline"
