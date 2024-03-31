7833bc60198d7ee8d677304c79f963bc6bb8ec70
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-31 18:09:39 -0400

    updating ollama

--- src/ollama-persistence/install.sh
+++ src/ollama-persistence/install.sh
@@ -2,26 +2,9 @@
 
 set -e
 
+EXTENSIONS=${EXTENSIONS:-}
 
-# Checks if packages are installed and installs them if not
-check_packages() {
-	if ! dpkg -s "$@" >/dev/null 2>&1; then
-		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-			echo "Running apt-get update..."
-			apt-get update -y
-		fi
-		apt-get -y install --no-install-recommends "$@"
-	fi
-}
-
-# make sure we have curl
-check_packages ca-certificates curl
-
-curl https://ollama.ai/install.sh | sh
-
-if [ ! -z "${MODELS}" ]; then
-
-  mkdir -p "/usr/local/share/ollama/"
+mkdir -p "/usr/local/share/ollama/"
 
 cat <<EOF > /usr/local/share/ollama/oncreate.sh
 #!/bin/env bash
@@ -35,6 +18,4 @@ done
 exit 0
 EOF
 
-  chmod 755 /usr/local/share/ollama/oncreate.sh
-
-fi
+chmod 755 /usr/local/share/ollama/oncreate.sh
