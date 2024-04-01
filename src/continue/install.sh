77e9d22adc3608c54357e8f592e2b6f3b06ed2fb
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-01 15:06:11 -0400

    feat(devcontainer-feature.json): add new devcontainer feature for Continue Extension docs(devcontainer-feature.js): create install script and devcontainer feature file for Continue Extension [create]: src/continue/install.sh: create new install script for Continue Extension [add]: src/container-dotfiles/devcontainer-feature.json: add dependency on Continue Extension feature refactor(src/container-dotfiles/devcontainer-feature.json): remove "installsAfter" property and update version number

---
+++ src/continue/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt-get update -y
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages curl
+
+if [ ! -d $_REMOTE_USER_HOME/.continue ];then
+  su -l $_REMOTE_USER -c "mkdir -p $_REMOTE_USER_HOME/.continue"
+fi
+
+su -l $_REMOTE_USER -c "curl -L https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.continue/config.json -o $_REMOTE_USER_HOME/.continue/config.json"
