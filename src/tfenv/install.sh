717c5ceb29e4dc4b8f32f9b2591733debe109f99
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-01 15:48:38 -0400

    refactor(devcontainer-feature.json): change "dependsOn" to "containerEnv" and add PATH env variable for tfenv bin update(devcontainer-feature.json): increment version number from 0.0.8 to 0.0.10 update(install.sh): remove unnecessary chown command and change git clone depth to shallow (--depth=1) to improve build performance

--- src/tfenv/install.sh
+++ src/tfenv/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-chown $_REMOTE_USER:$_REMOTE_USER -R $_REMOTE_USER_HOME
-
 if [ ! -d $_REMOTE_USER_HOME/.tfenv ];then
   su -l $_REMOTE_USER -c "git clone --depth=1 https://github.com/tfutils/tfenv.git $_REMOTE_USER_HOME/.tfenv"
 else
@@ -11,4 +9,3 @@ fi
 
 su -l $_REMOTE_USER -c "$_REMOTE_USER_HOME/.tfenv/bin/tfenv install"
 su -l $_REMOTE_USER -c "$_REMOTE_USER_HOME/.tfenv/bin/tfenv use"
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> $_REMOTE_USER_HOME/.bashrc
