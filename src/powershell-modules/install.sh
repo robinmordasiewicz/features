d48eeccbc709e4f284967e988b35ec779ac04a2d
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-02 18:27:00 -0400

    chore(powershell-modules): update devcontainer-feature.json and install.sh files
    1. In devcontainer-feature.json, increment version number from 0.0.7 to 0.0.8.
    2. In install.sh, add a line to read the MODULES environment variable as an array using IFS=',' and read -ra command.
    
    These changes were made to improve the modularity and maintainability of the project by keeping the version number up-to-date and allowing for easier management of multiple modules in the future.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 MODULES=${MODULES:-undefined}
+IFS=',' read -ra MODULES_ARRAY <<< "$MODULES"
 
 for mod in "${MODULES_ARRAY[@]}"; do
     pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
