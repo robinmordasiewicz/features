fe8feeb7b1af043bb839dcee76f6b39e8a8a163d
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-02 22:00:58 -0400

    chore(scripts/install.sh): add check\_packages function to install required packages and update package list before installation docs(src/oh-my-posh/devcontainer-feature.json): increment devcontainer-feature.json version number from 0.0.3 to 0.0.4 feat(src/oh-my-posh): update oh-my-posh package dependencies and add shellcheck pre-commit hook for better code quality checks

--- src/oh-my-posh/install.sh
+++ src/oh-my-posh/install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+# Checks if packages are installed and installs them if not
+check_packages () {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt-get update -y
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages curl
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
 if [ ! -d /usr/local/share/oh-my-posh ];then
   mkdir -p /usr/local/share/oh-my-posh
 fi
