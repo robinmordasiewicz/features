3202e25950312da1c5956ff2bb64be04d2958ebe
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-03 15:25:48 -0400

    commit(src/powershell-modules): update devcontainer-feature.json and install.sh scripts to support PowerShell modules array and improve installation process
    update devcontainer-feature.json file to version 0.0.17 and change "version" field to a string type
    update install.sh script to handle multiple PowerShell modules by using an array and looping through it for installation.
    
    This commit improves the handling of PowerShell modules in the container by allowing users to specify multiple modules to be installed during container creation. It also updates the devcontainer-feature.json file to support a newer version number and proper data type for the "version" field.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -1,15 +1,20 @@
 #!/bin/env bash
 set -e
 
-MODULES=${MODULES:-undefined}
+POWERSHELL_MODULES="${MODULES:-""}"
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
-MODULES="${MODULES}"
-IFS=',' read -ra MODULES_ARRAY <<< "\$MODULES"
-for mod in "\${MODULES_ARRAY[@]}"; do
-  pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
-  echo "pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
-done
+# If PowerShell modules are requested, loop through and install
+if [ ${#POWERSHELL_MODULES[@]} -gt 0 ]; then
+    echo "Installing PowerShell Modules: ${POWERSHELL_MODULES}"
+    modules=(`echo ${POWERSHELL_MODULES} | tr ',' ' '`)
+    for i in "${modules[@]}"
+    do
+        echo "Installing ${i}"
+        pwsh -NoProfile -NonInteractive -Command "Install-Module -Name ${i} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
+        echo "pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name ${i} -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
+    done
+fi
 
 # If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
 if [ -n "$POWERSHELL_PROFILE_URL" ]; then
@@ -27,9 +32,9 @@ cat <<EOF >/usr/local/share/powershell-modules/oncreate.sh
 #!/bin/env bash
 set -e
 
-MODULES="${MODULES}"
-IFS=',' read -ra MODULES_ARRAY <<< "\$MODULES"
-for mod in "\${MODULES_ARRAY[@]}"; do
+MODULES="${POWERSHELL_MODULES}"
+IFS=','
+for mod in "\${MODULES}"; do
   #sudo -n pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
   echo "sudo -n pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
 done
