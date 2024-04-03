a25974841071b3e584f0f88000f7bc7cdc0b5637
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-03 12:10:57 -0400

    refactor(devcontainer-feature.json, install.sh): add onCreateCommand to devcontainer-feature.json file and create oncreate.sh script in powershell-modules directory for automatic installation of modules during container creation docs(README.md): update instructions for setting up PowerShell environment with custom modules using the new oncreate.sh script and updated devcontainer-feature.json file.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -2,13 +2,6 @@
 set -e
 
 MODULES=${MODULES:-undefined}
-
-IFS=',' read -ra MODULES_ARRAY <<<"$MODULES"
-
-for mod in "${MODULES_ARRAY[@]}"; do
-  pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
-done
-
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
 # If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
@@ -18,3 +11,21 @@ if [ -n "$POWERSHELL_PROFILE_URL" ]; then
     profilePath=$(pwsh -noni -c '$PROFILE.AllUsersAllHosts')
     sudo -E curl -sSL -o "$profilePath" "$POWERSHELL_PROFILE_URL"
 fi
+
+if [ ! -d /usr/local/share/powershell-modules ];then
+  mkdir -p "/usr/local/share/powershell-modules/"
+fi
+
+cat <<EOF >/usr/local/share/powershell-modules/oncreate.sh
+#!/bin/env bash
+set -e
+
+MODULES="${MODULES}"
+IFS=',' read -ra MODULES_ARRAY <<< "\$MODULES"
+for ext in "\${MODULES_ARRAY[@]}"; do
+  pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
+done
+exit 0
+EOF
+
+chmod 755 /usr/local/share/powershell-modules/oncreate.sh
