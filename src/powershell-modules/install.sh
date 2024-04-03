f0ee72fed2a5f9a6ff50f4e6bba601d28bb15d8d
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-03 10:13:11 -0400

    chore(powershell-modules): update devcontainer-feature.json to use latest powershell image and remove powershell profile URL from installation script docs(powershell-modules): update version number in devcontainer-feature.json and install.sh files for new release
    fix(powershell-modules/install.sh): add check for POWERSHELLPROFILEURL environment variable before downloading the powershell profile file
    
    refactor(powershell-modules): separate the installation of the required powershell modules from the downloading and setting up of the powershell profile to improve maintainability and testability.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -2,8 +2,19 @@
 set -e
 
 MODULES=${MODULES:-undefined}
+
 IFS=',' read -ra MODULES_ARRAY <<<"$MODULES"
 
 for mod in "${MODULES_ARRAY[@]}"; do
   pwsh -NoProfile -NonInteractive -Command "Install-Module -Name $mod -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
 done
+
+POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
+
+# If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
+if [ -n "$POWERSHELL_PROFILE_URL" ]; then
+    echo "Downloading PowerShell Profile from: $POWERSHELL_PROFILE_URL"
+    # Get profile path from currently installed pwsh
+    profilePath=$(pwsh -noni -c '$PROFILE.AllUsersAllHosts')
+    sudo -E curl -sSL -o "$profilePath" "$POWERSHELL_PROFILE_URL"
+fi
