83e23846aecc3f079f3aca76ee2bd447802774f5
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-04 18:02:14 -0400

    chore(powershell-modules): update devcontainer-feature.json and install.sh scripts to use Install-Module command in a PowerShell script instead of running it directly
    refactor(powershell-modules): improve the installation process by creating a separate PowerShell script to install all required modules using Install-Module command and making it executable
    
    docs(powershell-modules): increment version number in devcontainer-feature.json file from 0.0.46 to 0.0.47 for the next release.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -4,13 +4,16 @@
 set -e
 
 MODULES=${MODULES:-undefined}
+echo "#!/usr/local/bin/pwsh -NoProfile" > /tmp/modules.ps1
 
 IFS=','
 for mod in ${MODULES}; do
   #pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
   #pwsh -NoProfile -noni -Command "& {Install-Module -Name ${mod} -AllowClobber -Force -Scope AllUsers}" || continue
-  pwsh -noni -Command "& {Install-Module -Name ${mod} -AllowClobber -Force -Scope AllUsers}" || continue
+  echo "Install-Module -Name ${mod} -AllowClobber -Force -Scope AllUsers" >> /tmp/modules.ps1
 done
+chmod 755 /tmp/modules.ps1
+/tmp/modules.ps1
 
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
