38cd23a4a456660c05f524d9790efcc5751a2b98
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-04 13:24:46 -0400

    refactor(devcontainer-feature.json, install.sh): update version number to 0.0.37 and remove redundant lines in install script docs(README.md): update version information in the installation instructions for Powershell modules

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -4,65 +4,15 @@ set -e
 MODULES=${MODULES:-undefined}
 echo "${MODULES}" > /tmp/powershell.txt
 
-echo '#!/usr/local/bin/pwsh -NoProfile' > /tmp/modules.ps1
-
-#IFS=',' read -ra MODULES <<<"${MODULES}"
-#for mod in ${MODULES[@]}; do
 IFS=','
 for mod in ${MODULES}; do
-  #pwsh -NoProfile -NonInteractive -Command ("Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers") || continue
-  #pwsh -NoProfile -NonInteractive -CommandWithArgs "Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
   pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
-  echo "Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" >> /tmp/modules.ps1
-done
-
-chmod 755 /tmp/modules.ps1
-#/tmp/modules.ps1
-#rm /tmp/modules.ps1
-
-#MODULES=${MODULES:-""}
-IFS=, read -ra MODULES <<< ${MODULES}
-for mod in ${MODULES[@]}
-do
-  pwsh -NoProfile -Command "Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
-  echo "sudo -n pwsh -NoProfile -Command \"Install-Module -Name \${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
 done
 
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
-# If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
-if [ ! -d "/opt/microsoft/powershell/7/" ];then
-   mkdir -p /opt/microsoft/powershell/7/
-fi
 
 if [ -n "$POWERSHELL_PROFILE_URL" ]; then
     echo "Downloading PowerShell Profile from: ${POWERSHELL_PROFILE_URL}"
-    # Get profile path from currently installed pwsh
     profilePath=$(pwsh -noni -c '$PROFILE.AllUsersAllHosts')
     sudo -E curl -sSL -o "${profilePath}" "${POWERSHELL_PROFILE_URL}" || continue
 fi
-
-exit 0
-
-if [ ! -d /usr/local/share/powershell-modules ];then
-  mkdir -p "/usr/local/share/powershell-modules/"
-fi
-
-
-cat <<EOF >/usr/local/share/powershell-modules/oncreate.sh
-#!/bin/bash
-set -e
-
-MODULES="${MODULES}"
-
-#IFS=, read -ra MODULES <<< "\${MODULES}"
-#for mod in \${MODULES[@]}
-IFS=','
-for mod in ${MODULES}; do
-do
-  #sudo -n pwsh -NoProfile -NonInteractive -Command "Install-Module -Name \${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
-  echo "sudo -n pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name \${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
-done
-exit 0
-EOF
-
-chmod 755 /usr/local/share/powershell-modules/oncreate.sh
