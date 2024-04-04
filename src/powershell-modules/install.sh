23d434d4a296481f7e56f8a2febdc8cc0e67925e
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-04 16:05:14 -0400

    commit (powershell-modules): increment devcontainer-feature.json version to 0.0.38 and update install.sh script to use non-interactive PowerShell
    fix(powershell-modules/devcontainer-feature.json): update version number in devcontainer-feature.json file
    refactor(powershell-modules/install.sh): modify curl command to run non-interactively and change PowerShell profile path retrieval method
    
    [Note: The commit message should be written as a single line, following the conventional commit format.]

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+#
+
 set -e
 
 MODULES=${MODULES:-undefined}
-echo "${MODULES}" > /tmp/powershell.txt
 
 IFS=','
 for mod in ${MODULES}; do
-  pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
+  pwsh -NoProfile -noni -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
 done
 
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
 if [ -n "$POWERSHELL_PROFILE_URL" ]; then
-    echo "Downloading PowerShell Profile from: ${POWERSHELL_PROFILE_URL}"
-    profilePath=$(pwsh -noni -c '$PROFILE.AllUsersAllHosts')
-    sudo -E curl -sSL -o "${profilePath}" "${POWERSHELL_PROFILE_URL}" || continue
+    profilePath=$(pwsh -noni -Command '$PROFILE.AllUsersAllHosts')
+    curl -sSL -o "${profilePath}" "${POWERSHELL_PROFILE_URL}" || continue
 fi
