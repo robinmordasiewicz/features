171a177e4bd7f68952ad291cc80ee02258923d71
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-04 00:55:38 -0400

    refactor(install.sh): change POWERSHELL\_MODULES variable initialization and loop to use array and avoid code duplication perf(install.sh): improve script performance by using a single assignment for MODULES variable and avoiding unnecessary variable assignments and command echoing.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-POWERSHELL_MODULES=${MODULES:-""}
-POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
-IFS=, read -ra MODULES <<< "${POWERSHELL_MODULES}"
-for mod in "${MODULES[@]}"
-do
+MODULES=${MODULES:-undefined}
+IFS=',' read -ra MODULES_ARRAY <<<"${MODULES}"
+for mod in "${MODULES_ARRAY[@]}"; do
   pwsh -NoProfile -NonInteractive -Command "Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
-  echo "sudo -n pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name \${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
 done
 
+#POWERSHELL_MODULES=${MODULES:-""}
+#IFS=, read -ra MODULES <<< "${POWERSHELL_MODULES}"
+#for mod in "${MODULES[@]}"
+#do
+#  pwsh -NoProfile -NonInteractive -Command "Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers" || continue
+#  echo "sudo -n pwsh -NoProfile -NonInteractive -Command \"Install-Module -Name \${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers\" || continue"
+#done
+
+POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 # If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
 if [ -n "$POWERSHELL_PROFILE_URL" ]; then
     echo "Downloading PowerShell Profile from: $POWERSHELL_PROFILE_URL"
