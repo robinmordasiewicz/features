17ccebd65513892454e736f14902bc3aef28ecfd
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-04 17:11:21 -0400

    chore(powershell-modules): update devcontainer-feature.json and install.sh files to use latest version numbers and correct path variable assignment for POWERSHELL\_PROFILE\_URL
    refactor(powershell-modules): modify script to check if POWERSHELL\_PROFILE\_URL is set as a variable instead of using a hardcoded string, and update version number in devcontainer-feature.json file.

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -7,12 +7,12 @@ MODULES=${MODULES:-undefined}
 
 IFS=','
 for mod in ${MODULES}; do
-  pwsh -NoProfile -noni -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
+  pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
 done
 
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
-if [ -n "$POWERSHELL_PROFILE_URL" ]; then
+if [ -n "${POWERSHELL_PROFILE_URL}" ]; then
   profilePath=$(pwsh -noni -Command '$PROFILE.AllUsersAllHosts')
   if [ ! -f "${profilePath}" ]; then
     curl -sSL -o "${profilePath}" "${POWERSHELL_PROFILE_URL}" || exit 0
