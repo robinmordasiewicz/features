d8fe7c78c99651675e8e64699b5288a509b7e9fa
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-05 10:23:11 -0400

    chore(devcontainer-feature.json, install.sh): update devcontainer-feature.json to use 'ls' command instead of 'sudo' and add 'Force' flag in Install-Module command for powershell-modules installation script docs(README.md): update the installation instructions for the powershell-modules to reflect the changes made in the chore commit above

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -9,8 +9,8 @@ if [ ! -d "/usr/local/share/powershell-modules/" ]; then
   mkdir -p "/usr/local/share/powershell-modules/"
 fi
 
-echo "#!/opt/microsoft/powershell/7/pwsh -NoProfile" >/usr/local/share/powershell-modules/modules.ps1
-echo "Set-PSRepository -Name PSGallery -InstallationPolicy Trusted" >>/usr/local/share/powershell-modules/modules.ps1
+#echo "#!/opt/microsoft/powershell/7/pwsh -NoProfile" >/usr/local/share/powershell-modules/modules.ps1
+echo "Set-PSRepository -Name PSGallery -InstallationPolicy Trusted" >/usr/local/share/powershell-modules/modules.ps1
 #pwsh -NoProfile -Command '& {Set-PSRepository -Name PSGallery -InstallationPolicy Trusted'
 #pwsh -NoProfile -Command '& {Install-Module -Name z -Repository PSGallery -AllowClobber -Scope AllUsers}'
 #pwsh -NoProfile -Command '& {Install-Module -Name Terminal-Icons -Repository PSGallery -AllowClobber -Scope AllUsers}'
@@ -20,10 +20,11 @@ IFS=','
 for mod in ${MODULES}; do
   #pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
   #pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -AllowClobber -Force -Scope AllUsers}" || continue
-  echo "Install-Module -Name ${mod} -AllowClobber -Scope AllUsers" >>/usr/local/share/powershell-modules/modules.ps1
+  echo "Install-Module -Name ${mod} -AllowClobber -Scope AllUsers -Force" >>/usr/local/share/powershell-modules/modules.ps1
 done
 
 chmod 755 /usr/local/share/powershell-modules/modules.ps1
+/opt/microsoft/powershell/7/pwsh -NoProfile -command . "/usr/local/share/powershell-modules/modules.ps1"
 
 POWERSHELL_PROFILE_URL="${POWERSHELLPROFILEURL}"
 
