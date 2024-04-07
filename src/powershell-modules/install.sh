4c0665f2063481c0a2c4fec8ed083986800f84ea
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-07 07:39:23 -1000

    chore(devcontainer-feature.json): update devcontainer-feature.json version number to 0.0.70 chore(install.sh): add "Write-Host 'Hello, World!'" message in modules.ps1 for testing purposes docs(devcontainer-feature.json): update version number in devcontainer-feature.json file for documentation consistency

--- src/powershell-modules/install.sh
+++ src/powershell-modules/install.sh
@@ -24,8 +24,9 @@ for mod in ${MODULES}; do
   #pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -Repository PSGallery -AllowClobber -Force -Scope AllUsers}" || continue
   #pwsh -NoProfile -Command "& {Install-Module -Name ${mod} -AllowClobber -Force -Scope AllUsers}" || continue
   #echo "Install-Module -Name ${mod} -AllowClobber -Scope AllUsers -Force" >>/usr/local/share/powershell-modules/modules.ps1
-  echo "Get-InstalledModule" >>/usr/local/share/powershell-modules/modules.ps1
+  #echo "Get-InstalledModule" >>/usr/local/share/powershell-modules/modules.ps1
 done
+echo "Write-Host 'Hello, World!'" >/usr/local/share/powershell-modules/modules.ps1
 
 chmod 755 /usr/local/share/powershell-modules/modules.ps1
 /opt/microsoft/powershell/7/pwsh -nop -noni -command . "/usr/local/share/powershell-modules/modules.ps1"
