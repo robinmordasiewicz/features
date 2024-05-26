5a6cbd51c3ea140205320c63441dc3d046eb0dc3
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-05-26 15:26:39 -0400

    fix: adjust krew path

--- src/krew/install.sh
+++ src/krew/install.sh
@@ -5,6 +5,8 @@ set -x; cd "$(mktemp -d)" &&
 OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
 KREW="krew-${OS}_${ARCH}" &&
-curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-tar zxvf "${KREW}.tar.gz" &&
-./"${KREW}" install krew
+curl -o /tmp/${KREW}.tar.gz -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+tar zxvf "/tmp/${KREW}.tar.gz" -C /tmp &&
+su -l "${_REMOTE_USER}" -c "/tmp/${KREW} install krew" &&
+rm /tmp/${KREW}.tar.gz &&
+rm /tmp/${KREW}
