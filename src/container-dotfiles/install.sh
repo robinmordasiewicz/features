81ef6e03e2cbb0f0c83a0cba779ef1b81b40f9a8
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-06-13 11:44:37 -0400

    add flux to kubernetes

--- src/container-dotfiles/install.sh
+++ src/container-dotfiles/install.sh
@@ -49,3 +49,11 @@ fi
 
 #su -l "${_REMOTE_USER}" -c "echo '(gh auth status >/dev/null 2>&1 | grep -q \"Logged in to github.com account\") || gh auth login' >> ${_REMOTE_USER_HOME}/.bashrc"
 #su -l "${_REMOTE_USER}" -c "echo '(gh auth status >/dev/null 2>&1 | grep -q \"Logged in to github.com account\") || gh auth login' >> ${_REMOTE_USER_HOME}/.zshrc"
+
+su -l "${_REMOTE_USER}" -c "mkdir -p ~/.local/state/vs-kubernetes/tools/helm/linux-amd64/"
+su -l "${_REMOTE_USER}" -c "mkdir -p ~/.local/state/vs-kubernetes/tools/kubectl/linux-amd64/"
+su -l "${_REMOTE_USER}" -c "mkdir -p ~/.local/state/vs-kubernetes/tools/minikube/linux-amd64/"
+
+su -l "${_REMOTE_USER}" -c "ln -s /usr/bin/helm ~/.local/state/vs-kubernetes/tools/helm/linux-amd64/helm"
+su -l "${_REMOTE_USER}" -c "ln -s /usr/bin/kubectl ~/.local/state/vs-kubernetes/tools/kubectl/linux-amd64/kubectl"
+su -l "${_REMOTE_USER}" -c "ln -s /usr/bin/minikube ~/.local/state/vs-kubernetes/tools/helm/linux-amd64/minikube"
