9d08b6fc567a80d86904351f094bebebff3f6b17
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-11-21 12:33:56 -0500

    add sample feature

--- src/cmctl/install.sh
+++ src/cmctl/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-LATEST_VERSION=$(curl -s "https://api.github.com/repos/cert-manager/cmctl/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-ARCHTYPE=$(dpkg-architecture -q DEB_BUILD_ARCH)
-
-curl -L -o cmctl "https://github.com/cert-manager/cmctl/releases/download/v${LATEST_VERSION}/cmctl_linux_${ARCHTYPE}"
+OS=$(uname -s | tr A-Z a-z)
+ARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')
+curl -fsSL -o cmctl https://github.com/cert-manager/cmctl/releases/latest/download/cmctl_${OS}_${ARCH}
+chmod +x cmctl
 install cmctl /usr/local/bin
 rm cmctl
